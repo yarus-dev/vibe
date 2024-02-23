d262702ce27565e36ddef4a4b707835d4beed336
Yaroslav Usenko <yar.usenko.inc@gmail.com>
2024-02-23 19:22:18 +0200

    fix: footer and remove unnesesery component

--- apps/web/src/partials/Footer.jsx
+++ apps/web/src/partials/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer({ children, ...props }) {
   return (
     <footer className="container max-w-prose mx-auto text-center mb-3 mt-3">
-      Made with ❤️ by by{" "}
+      Made with&nbsp;❤️&nbsp;by{" "}
       <a className="text-blue-600 underline" href="https://yarus.app">
         Yaroslav Usenko
       </a>
