c9606fac82cab288b7d02ab5ff4344d95607e8fd
Yaroslav Usenko <yar.usenko.inc@gmail.com>
2024-03-02 14:49:56 +0200

    fix admin for vercel

--- apps/admin/src/App.jsx
+++ apps/admin/src/App.jsx
@@ -1,32 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { JournalPage } from "@/pages/Journal";
 // import { LoadingPage } from "@/pages/Loading";
 import { LoginPage } from "@/pages/Login";
-import { SettingsPage } from "@/pages/Settings";
 
 
 const router = createBrowserRouter(
-  import.meta.env.PROD
-    ? [
+    [
         {
           id: "root",
           path: "/",
           Component: LoginPage,
         },
       ]
-    : [
-        {
-          Component: Layout,
-          children: [
-            { index: true, element: <JournalPage /> },
-            { path: "/profile", element: <SettingsPage /> },
-          ],
-        },
-        {
-          path: "/login",
-          element: <LoginPage />,
-        },
-      ],
 );
 
 if (import.meta.hot) {
